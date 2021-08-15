131fc9b5b625bb86a80cb6535fff00a95d89912f
Machos <valkaniotist@gmail.com>
2021-08-15 13:55:21 +0300

    created view object

---
+++ backend/kb_backend/models/View.py
@@ -0,0 +1,10 @@
+from kb_backend.models.Campaign import Campaign
+from django.db import models
+import uuid
+from django.contrib.auth.models import User
+
+class View(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    link = models.CharField(max_length=512)
+    create_date = models.DateTimeField(auto_now_add=True)
+    campaign = models.ForeignKey(Campaign,on_delete=models.CASCADE)
