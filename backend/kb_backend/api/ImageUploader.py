659641d9494bb0508a9ec63f93dc114ec368e80f
Machos <valkaniotist@gmail.com>
2023-02-23 18:49:43 +0200

    Ok

--- backend/kb_backend/api/ImageUploader.py
+++ backend/kb_backend/api/ImageUploader.py
@@ -4,27 +4,48 @@ from rest_framework.response import Response
 from rest_framework import generics
 from PIL import Image as PILImage
 from ..serializers import ImageSerializer
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 
 class ImageUploader(generics.CreateAPIView):
     serializer_class = ImageSerializer
     parser_classes = [MultiPartParser, FormParser]
 
-    def create(self, request, *args, **kwargs):
-      image = request.data['image']
-      print(type(image))
-      image = PILImage.open(image)
-      max_size = (400, 400)
-      if image.height > 800 or image.width > 800:
-        image.thumbnail(max_size)
-      image_io = BytesIO()
-      image.save(image_io, format='JPEG')
-      image_io.seek(0)
-      #print(image_io.read())
-      request.data['image'] = image_io.read()
-      serializer = self.get_serializer(data=request.data)
-      if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=201)
-      return Response(serializer.errors, status=400)
+    def post(self, request, *args, **kwargs):
+      # image = request.data['image']
+      # image = PILImage.open(image)
+      # max_size = (200, 200)
+      # if image.height > 200 or image.width > 200:
+      #   image.thumbnail(max_size)
+      # image_io = BytesIO()
+      # image.save(image_io, format='JPEG')
+      # image_io.seek(0)
+
+      # uploaded_file = InMemoryUploadedFile(
+      #   file=image_io,
+      #   field_name='image',
+      #   name='image.png',
+      #   content_type='image/png',
+      #   size=len(image_io.getvalue()),
+      #   charset=None,
+      # )
+        image_file = request.data.get('image')
+        image = PILImage.open(image_file)
+        max_size = (200, 200)
+        if image.height > 200 or image.width > 200:
+          image.thumbnail(max_size)
+        image_io = BytesIO()
+        image.save(image_io, format='JPEG')
+        image_io.seek(0)
+        
+        if image_file:
+            file_name = default_storage.save("images/"+image_file.name, ContentFile(image_io.read()))
+            file_url = default_storage.url(file_name)
+            image_io.close()
+            return Response({'file_name': file_name, 'file_url': file_url}, status=201)
+        else:
+            image_io.close()
+            return Response({'error': 'No image file provided.'}, status=400)
 
 
