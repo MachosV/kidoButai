32d091f2eb98eda3614c4eb745e6776c5117e1ec
Machos <valkaniotist@gmail.com>
2023-02-23 20:10:42 +0200

    fixed images, working as intended

--- backend/kb_backend/api/ImageUploader.py
+++ backend/kb_backend/api/ImageUploader.py
@@ -7,6 +7,9 @@ from ..serializers import ImageSerializer
 from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+import hashlib
+import uuid
+
 
 class ImageUploader(generics.CreateAPIView):
     serializer_class = ImageSerializer
@@ -32,18 +35,29 @@ class ImageUploader(generics.CreateAPIView):
       # )
         image_file = request.data.get('image')
         image = PILImage.open(image_file)
+        # try:
+        #   image.verify()
+        # except:
+        #   return Response({'error': 'Invalid image format'}, status=400)
+
+        if not image.format.lower() in ['png', 'jpg', 'jpeg', 'tiff', 'bmp', 'gif']:
+          return Response({'error': 'Invalid image format'}, status=400)
+
+
         max_size = (200, 200)
         if image.height > 200 or image.width > 200:
           image.thumbnail(max_size)
+        
         image_io = BytesIO()
-        image.save(image_io, format='JPEG')
+        newImageFilename = str(hashlib.md5(image.filename.encode('utf-8')).hexdigest())[0:4]+str(uuid.uuid4())[-8:-1]
+        image.save(image_io, filename = newImageFilename, format = image.format.lower())
         image_io.seek(0)
         
         if image_file:
-            file_name = default_storage.save("images/"+image_file.name, ContentFile(image_io.read()))
+            file_name = default_storage.save("images/"+newImageFilename+"."+image.format.lower(), ContentFile(image_io.read()))
             file_url = default_storage.url(file_name)
             image_io.close()
-            return Response({'file_name': file_name, 'file_url': file_url}, status=201)
+            return Response({'file_url': "http://localhost:8000"+file_url}, status=201)
         else:
             image_io.close()
             return Response({'error': 'No image file provided.'}, status=400)
