32131694c0661add9097dc30f9dba87877b148f1
Machos <valkaniotist@gmail.com>
2023-02-26 21:49:27 +0200

    change user password

--- backend/kb_backend/api/AuthViews.py
+++ backend/kb_backend/api/AuthViews.py
@@ -3,8 +3,9 @@ from django.http.response import JsonResponse
 from rest_framework.views import APIView
 from django_expiring_token.models import ExpiringToken
 from django_expiring_token.authentication import ExpiringTokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
+from rest_framework import status
+from django.contrib.auth.models import User
+import string
 
 class AuthView(APIView):
     def post(self, request):
@@ -37,9 +38,82 @@ class CheckAuth(APIView):
     authentication_classes = [ExpiringTokenAuthentication]
 
     def get(self, request, format=None):
-        content = content = {
+        content = {
             'response':'Not authed',  # None
             }
         if (request.user.is_authenticated):
             content["response"] = "Authed"
         return JsonResponse(content)
+
+class ChangePassword(APIView):
+    authentication_classes = [ExpiringTokenAuthentication]
+
+    def post(self, request):
+
+        response = {
+            "message":[],
+            "errors":[]
+        }
+
+        passwordRequirements = {
+            "oldPassAndNewPassNotSame": False,
+            "oldPasswordOK": False,
+            "passwordsMatch": False,
+            "passwordLengthOK": False,
+            "passwordHasCapitalChar": False,
+            "passwordHasSpecialChar": False,
+        }
+
+
+        #check if old password is valid
+        if request.user.check_password(request.data["oldPassword"]):
+            passwordRequirements["oldPasswordOK"] = True
+        else:
+            response["errors"].append("Old password invalid")
+
+
+        #check if passwords match
+        if request.data["newPassword"] == request.data["newPasswordConfirm"]:
+            passwordRequirements["passwordsMatch"] = True
+        else:
+            response["errors"].append("Passwords do not match")
+
+        #check if old password is the same with the new password
+        if request.data["newPassword"] == request.data["oldPassword"]:
+            response["errors"].append("New password cannot be the same as the new password")
+        else:
+            passwordRequirements["oldPassAndNewPassNotSame"] = True
+
+        #check if password length is over 7 characters
+        if len(request.data["newPassword"]) > 7 and len(request.data["newPasswordConfirm"]) > 7:
+            passwordRequirements["passwordLengthOK"] = True
+        else:
+            response["errors"].append("Password length must be over 7 characters")
+
+        #check if password has capital character
+        if any(c.isupper() for c in request.data["newPassword"]) and any(c.isupper() for c in request.data["newPasswordConfirm"]):
+            passwordRequirements["passwordHasCapitalChar"] = True
+        else:
+            response["errors"].append("Password must contain at least 1 capital character")
+
+        if any(c in string.punctuation for c in request.data["newPassword"]) and any(c in string.punctuation for c in request.data["newPasswordConfirm"]):
+            passwordRequirements["passwordHasSpecialChar"] = True
+        else:
+           response["errors"].append("Password must contain at least 1 special character") 
+
+
+        #check all password requirements
+        #if some fails, return an error response
+        for policy in passwordRequirements:
+            if passwordRequirements[policy] == False:
+                response["message"] = "Please verify the password requirements"
+                return JsonResponse(response,status = status.HTTP_400_BAD_REQUEST)
+
+        #change the users password
+        response["message"] = "Password changed successfuly!"
+
+        u = User.objects.get(username=request.user)
+        u.set_password(request.data["newPassword"])
+        u.save()
+
+        return JsonResponse(response,status=status.HTTP_200_OK)
--- backend/kb_backend/apiUrls.py
+++ backend/kb_backend/apiUrls.py
@@ -14,8 +14,7 @@ urlpatterns = [
     path('campaign/create', CampaignViews.CampaignCreateEndpoint.as_view() , name='CampaignCreateEndpoint'),
     path('campaign/<pk>', CampaignViews.CampaignEndpoint.as_view() , name='CampaignRUDEndpoint'),
     path('campaign/<pk>/update', CampaignViews.CampaignUpdateEndpoint.as_view() , name='CampaignUEndpoint'),
-    path('stats/<pk>/weekly',ViewViews.ViewListWeeklyEndpoint.as_view(),name="ViewListWeekly"),
-    path('stats/<pk>/daily',ViewViews.ViewListDailyEndpoint.as_view(),name="ViewListDaily"),
+    path('auth/changePassword',AuthViews.ChangePassword.as_view(), name='ChangePassword'),
+    path('auth/checkAuth',AuthViews.CheckAuth.as_view(), name='CheckAuthView'),
     path('auth',AuthViews.AuthView.as_view(), name='AuthenticationView'),
-    path('checkAuth',AuthViews.CheckAuth.as_view(), name='CheckAuthView'),
 ]
